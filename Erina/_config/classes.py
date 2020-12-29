0d1d15c10c861f480be9c0ccd83f817d9f9b8518
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2020-12-29 04:47:53 +0100

    [temp fix] integer port

--- Erina/_config/classes.py
+++ Erina/_config/classes.py
@@ -19,7 +19,7 @@ def environ(erina_environ):
     Returns an env variable if it has the correct erina environ format
     """
     if erina_environ == "{{PORT}}":
-        return os.environ.get("PORT")
+        return int(os.environ.get("PORT"))
     if str(erina_environ)[:2] == "{{" and str(erina_environ)[-2:] == "}}":
         if re.sub("[^0-9.-]", "", str(erina_environ)) == "":
             return convert_to_float(os.environ.get(str(erina_environ)[2:-2]))
